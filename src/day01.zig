125c4aed8b28faf85fc3e82d7b82b4cc6269dd77
tommy-donavon <donavontommy@gmail.com>
2024-12-01 17:34:34 -0700

    feat: day1

--- src/day01.zig
+++ src/day01.zig
@@ -1,40 +1,65 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
 
-const util = @import("util.zig");
-const gpa = util.gpa;
+const List = std.ArrayList;
+const NumList = List(isize);
+
+const print = std.debug.print;
+const tokenizeSeq = std.mem.tokenizeSequence;
+const tokenizeSca = std.mem.tokenizeScalar;
+const parseInt = std.fmt.parseInt;
 
 const data = @embedFile("data/day01.txt");
 
-pub fn main() !void {}
+const Solution = struct {
+    fn populateLists(first: *NumList, second: *NumList) !void {
+        var lines = tokenizeSca(u8, data, '\n');
+        while (lines.next()) |line| {
+            var segments = tokenizeSca(u8, line, ' ');
+            var index: usize = 0;
+            while (segments.next()) |seg| : (index += 1) {
+                const num = try parseInt(isize, seg, 10);
+                try if (index == 0) first.append(num) else second.append(num);
+            }
+        }
+    }
 
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
+    fn part1(alloc: std.mem.Allocator) !usize {
+        var listA = NumList.init(alloc);
+        var listB = NumList.init(alloc);
+        try populateLists(&listA, &listB);
 
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
+        std.mem.sort(isize, listA.items, {}, std.sort.asc(isize));
+        std.mem.sort(isize, listB.items, {}, std.sort.asc(isize));
 
-const print = std.debug.print;
-const assert = std.debug.assert;
+        var sum: usize = 0;
+        for (listA.items, listB.items) |a, b| {
+            sum += @abs(a - b);
+        }
+        return sum;
+    }
 
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
+    fn part2(alloc: std.mem.Allocator) !usize {
+        var listA = NumList.init(alloc);
+        var listB = NumList.init(alloc);
+        try populateLists(&listA, &listB);
+
+        var sum: usize = 0;
+
+        for (listA.items) |elm| {
+            const needle = [_]isize{elm};
+            const count = std.mem.count(isize, listB.items, &needle);
+            sum += count * @as(usize, @intCast(elm));
+        }
+
+        return sum;
+    }
+};
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    print("Part 1 Solution: {d}\n", .{try Solution.part1(alloc)});
+    print("Part 2 Solution: {d}\n", .{try Solution.part2(alloc)});
+}
